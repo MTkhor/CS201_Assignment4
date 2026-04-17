2d5133e2208aefc561a5efbed724376be736ea16
MTkhor <myloslava.tkhor.25@kse.org.ua>
2026-04-17 15:27:49 +0200

    Added graph of random walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import json
+import matplotlib.pyplot as plt
 
 #Reading data
 df = pd.read_csv('random_walk.csv')
@@ -16,3 +16,19 @@ filtered_data = df[df["distance"] > stats["mean"]]
 print(filtered_data)
 
 filtered_data.to_json("filtered_walk.json")
+
+
+# Plotting data
+first_point = [df["x"][0], df["y"][0]]
+last_point = [df["x"][len(df) - 1], df["y"][len(df) - 1]]
+
+plt.figure(figsize=(8, 4))
+plt.plot(df["x"], df["y"], color='pink', marker='none', linestyle='-', label="random wak trajectory")
+plt.plot(first_point[0], first_point[1], color='red', marker='$\heartsuit$', linestyle='none', label="starting point")
+plt.plot(last_point[0], last_point[1], color='purple', marker='$\heartsuit$', linestyle='none', label="ending point")
+plt.title("Random walk example")
+plt.xlabel("x coordinate")
+plt.ylabel("y coordinate")
+plt.grid(True)
+plt.legend()
+plt.show()
